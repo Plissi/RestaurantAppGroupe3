afbb1dd61d9bf148b25b54a07ef978350adba988
Maeva-DEMANOU <maeva.demanou.cm@viacesi.fr>
2020-12-11 17:04:25 +0100

    ajout classe ModeleTest

---
+++ ModeleTest/MaitreHotelTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModeleTest
+{
+    class MaitreHotelTest
+    {
+        static void main (string[] ars)
+        {
+            int nombreClient = 0;
+            int resultat = AccueillirClient(nombreClient);
+            if (resultat < 1)
+            {
+                Console.WriteLine("le test a echoué");
+            }
+        }
+
+        private static int AccueillirClient(int a)
+        {
+            return 0;
+        }
+    }
+}
